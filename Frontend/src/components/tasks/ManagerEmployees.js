c151b5d97aae4791265993affc9829e2f41456c4
ganesh0455 <ganesh.c@westagilelabs.com>
2022-10-12 19:24:54 +0530

    Frontend code uploaded

---
+++ Frontend/src/components/tasks/ManagerEmployees.js
@@ -0,0 +1,142 @@
+import axios from "axios";
+import React, { useState,useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+function ManagerEmps(){
+    const navigate = useNavigate();
+    const loggedinUser = JSON.parse(localStorage.getItem('LoggedInUser'));
+    const jwt = JSON.parse(localStorage.getItem('jwtToken'));
+    const [ManagerEmps,setManagerEmps]=useState([]);
+    const [managerEmpTask,setManagerEmpTask]=useState([]);
+    const [capturedId,setCapturedId]=useState('');
+    const [stateEmpName,setStateEmpName]=useState('');
+    const [empId,setEmpId]=useState('');
+    const [getRole,setGetRole]=useState('');
+    useEffect(() => {
+        //for jwt token
+        if(jwt===null){
+            navigate('/login')
+            window.location.reload(true);
+        }
+    }, [jwt]);
+
+    const gdoId=loggedinUser.gdoId;
+    useEffect(()=>{
+        axios.get(`http://localhost:8001/managerEmps?gdoId=${gdoId}`)
+        .then((res)=>{
+            setManagerEmps(res.data.data);
+        })
+        .catch((error)=>{
+            console.log(error)
+        })
+    },[])
+    
+    async function handleEmpOutside(clickedId,role)
+    {
+        axios.get(`http://localhost:8001/empPendingTasksAtManagerOrAdmin?empId=${clickedId}&role=${role}`)
+            .then(res => {
+            var resdata = res.data;
+                setManagerEmpTask(resdata.data);
+            })
+            .catch(err => {
+            console.log(err);
+        })
+    }
+    return(
+        <div style={{backgroundColor:"#B9DCEC",minHeight:"100vh"}}>
+            <div>
+                <nav className="navbar navbar-inverse" style={{height:"53px"}}>
+                    <div className="container-fluid">
+                        <ul className="nav navbar-nav">
+                        <li><a style={{color:"#B9DCEC",cursor:"pointer"}} onClick={()=>{navigate('/viewTasks')}}>Home</a></li>
+                        <li><a style={{pointerEvents:"none",marginLeft:"770px",color:"white"}}>{loggedinUser?.name}</a></li>
+                        <li><a style={{pointerEvents:"none",color:"white"}}>{loggedinUser.gdo.gdoName}</a></li>
+                        <li><a style={{pointerEvents:"none",color:"white"}}>{loggedinUser.project.projName}</a></li>
+                        <li><a style={{cursor:"pointer",color:"red"}} onClick={() => { localStorage.clear(); navigate('/login')}}>Logout</a></li>
+                        </ul>
+                    </div>
+                </nav>
+            </div>
+            <div style={{backgroundColor:"#B9DCEC",display:'flex'}}>
+                <div style={{ height: '300px', width: '400px', boxShadow: '0 0 2px 2px', marginLeft: '120px', marginTop: "170px", borderRadius: '10px',backgroundColor:"#E4E4E4"}}>
+                   <table className="table table-hover">
+                        <thead style={{fontSize:"18px"}}>
+                            <tr>
+                                <th>Employee</th>
+                                <th>Project Name</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {ManagerEmps && ManagerEmps.map((emp,i)=>{
+                                const getRole=loggedinUser.role.roleName;
+                                const handleEmp=event=>{
+                                    setStateEmpName(emp.name);
+                                    setEmpId(emp.id);
+                                    setGetRole(getRole);
+                                    handleEmpOutside(emp.id,getRole);
+                                }
+                                return(
+                                    <tr>
+                                        <td onClick={handleEmp} style={{cursor:"pointer"}}>{emp.name}</td>
+                                        <td>{emp.project.projName}</td>
+                                    </tr>
+                                );
+                            })}
+                        </tbody>
+                   </table>
+                </div>
+                <div style={{ height: '570px', width: '700px', boxShadow: '0 0 2px 2px', marginLeft: '70px', borderRadius: '10px',backgroundColor:"#E4E4E4",overflow: "scroll"}}>
+                    <h2 style={{textAlign:"center"}}>{stateEmpName}</h2>
+                    <div style={{marginLeft:"90px"}}><table className="table table-hover">
+                        <thead style={{fontSize:"18px"}}>
+                            <tr>
+                                <th>Task</th>
+                                <th>Date</th>
+                            <tr>
+                                <th></th>
+                            </tr>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {managerEmpTask && managerEmpTask.map((task,i)=>{
+                                const handleApproveReject=event=>{
+                                    let status;
+                                    if(event.target.value==='1'){
+                                        status='Approved';
+                                    }
+                                    else if(event.target.value==='2'){
+                                        status='Rejected';
+                                    }
+                                    const clickedId=task.id;
+                                    const roleName=loggedinUser.role.roleName;
+                                    axios.put(`http://localhost:8001/ApproveORreject?taskId=${clickedId}&roleName=${roleName}&status=${status}`)
+                                    .then((res)=>{
+                                        handleEmpOutside(empId,getRole);
+                                    })
+                                    .catch((error)=>{
+                                        console.log(error);
+                                    })
+                                }
+                                return(
+                                    <tr>
+                                        <td>{task.tasks}</td>
+                                        <td>{task.date}</td>
+                                        <tr>
+                                            <button value="1" className="btn btn-success" onClick={handleApproveReject}>Approve</button>
+                                            <button value="2" className="btn btn-danger" style={{marginLeft:"15px"}} onClick={handleApproveReject}>Reject</button>
+                                        </tr>
+                                    </tr>
+                                );
+                            })}
+                        </tbody>
+                    </table></div>
+                </div>
+            </div>
+            <ToastContainer autoClose={500} />
+        </div>
+    );
+}
+
+export default ManagerEmps;
---
+++ Frontend/src/components/tasks/ManagerUnderAdmin.js
@@ -0,0 +1,237 @@
+import axios from "axios";
+import React, { useState } from "react";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+function ManagerUnderAdmin(){
+    const navigate=useNavigate();
+    const [AllManagers,setAllManagers]=useState([]);
+    const [EmpsUnderManager,setEmpsUnderManager]=useState([]);
+    const [showEmpDiv,setShowEmpDiv]=useState(false);
+    const [clickedManagerName,setClickedManagerName]=useState('');
+    const [managerTasks,setManagerTasks]=useState([]);
+    const [empTasksPendingAtAdmin,setEmpTasksPendingAtAdmin]=useState([]);
+    const [showManagerTasks,setShowManagerTasks]=useState(true);
+    const [showEmpTasks,setShowEmpTasks]=useState(false);
+    const [clickedEmpName,setClickedEmpName]=useState('');
+    const [stateManagerId,setGetManagerId]=useState('');
+    const [stateEmpId,setGetEmpId]=useState('');
+    const [stateRole,setGetRole]=useState('');
+    const loggedinUser = JSON.parse(localStorage.getItem('LoggedInUser'));
+    const jwt = JSON.parse(localStorage.getItem('jwtToken'));
+
+    useEffect(() => {
+        //for jwt token
+        if(jwt===null){
+            navigate('/login')
+            window.location.reload(true);
+        }
+    }, [jwt]);
+
+    useEffect(()=>{
+        axios.get('http://localhost:8001/allManagers')
+        .then((response)=>{
+            const resdata=response.data;
+            setAllManagers(resdata.data);
+        })
+        .catch((error)=>{
+            console.log(error);
+        })
+    },[]);
+
+    async function handleManagerTasksOutside(getManagerId)
+    {
+        axios.get(`http://localhost:8001/managerPendingTasksAtAdmin?managerId=${getManagerId}`)
+        .then((response)=>{
+            const resdata=response.data;
+            setManagerTasks(resdata.data);
+        })
+        .catch((error)=>{
+            console.log(error);
+        })
+    }
+
+    async function handleEmpNameOutside(empId,getRole)
+    {
+        axios.get(`http://localhost:8001/empPendingTasksAtManagerOrAdmin?empId=${empId}&role=${getRole}`)
+        .then((response)=>{
+            const resdata=response.data;
+            setEmpTasksPendingAtAdmin(resdata.data);
+            setShowManagerTasks(false);
+            setShowEmpTasks(true);
+        })
+        .catch((error)=>{
+            console.log(error);
+        })
+    }
+    return(
+        <div style={{backgroundColor:"#B9DCEC",minHeight:"100vh"}}>
+            <div>
+                <nav className="navbar navbar-inverse" style={{height:"53px"}}>
+                    <div className="container-fluid">
+                        <ul className="nav navbar-nav">
+                        <li><a style={{color:"#28FF01"  ,cursor:"pointer"}} onClick={()=>{navigate('/viewTasks')}}>Home</a></li>
+                        <li><a style={{color:"#B9DCEC",cursor:"pointer"}} onClick={()=>{navigate('/GdoProjects')}}>Gdo & Projects</a></li>
+                        <li><a style={{pointerEvents:"none",marginLeft:"600px",color:"white"}}>{loggedinUser?.name}</a></li>
+                        <li><a style={{pointerEvents:"none",color:"white"}}>{loggedinUser.gdo.gdoName}</a></li>
+                        <li><a style={{pointerEvents:"none",color:"white"}}>{loggedinUser.project.projName}</a></li>
+                        <li><a style={{cursor:"pointer",color:"red"}} onClick={() => { localStorage.clear(); navigate('/login')}}>Logout</a></li>
+                        </ul>
+                    </div>
+                </nav>
+            </div>
+            <div style={{display:'flex',maxHeight:'100%'}}>
+                <div >
+                    <div style={{ height: '300px', width: '400px', boxShadow: '0 0 2px 2px', marginLeft: '20px', borderRadius: '10px',backgroundColor:"#E4E4E4",marginBottom:"10px",overflow:"scroll"}}>
+                        <table className="table table-hover">
+                            <thead style={{fontSize:"18px"}}>
+                                <tr>
+                                    <th>Name</th>
+                                    <th>Project</th>
+                                    <th>Gdo</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {AllManagers && AllManagers.map((manager,i)=>{
+                                    const handleManagerName=event=>{
+                                        setShowEmpDiv(true);
+                                        setShowManagerTasks(true);
+                                        setShowEmpTasks(false);
+                                        setClickedManagerName(manager.name)
+                                        const getGdoId=manager.gdo.id;
+                                        axios.get(`http://localhost:8001/managerEmps?gdoId=${getGdoId}`)
+                                        .then((res)=>{
+                                            setEmpsUnderManager(res.data.data);
+                                        })
+                                        .catch((error)=>{
+                                            console.log(error)
+                                        })
+                                        const getManagerId=manager.id;
+                                        setGetManagerId(getManagerId);
+                                        handleManagerTasksOutside(getManagerId);
+                                    }
+                                    return(
+                                        <tr>
+                                            <td onClick={handleManagerName} style={{cursor:"pointer"}}>{manager.name}</td>
+                                            <td>{manager.project.projName}</td>
+                                            <td>{manager.gdo.gdoName}</td>
+                                        </tr>
+                                    );
+                                })}
+                            </tbody>
+                        </table>
+                    </div>
+                    {showEmpDiv && <div style={{ height: '260px', width: '400px', boxShadow: '0 0 2px 2px', marginLeft: '20px', borderRadius: '10px',backgroundColor:"#E4E4E4",overflow:"scroll"}}>
+                        <h3 style={{textAlign:"center"}}>Employees under {clickedManagerName}</h3>
+                        <table className="table table-hover">
+                            <thead style={{fontSize:"18px"}}>
+                                <tr>
+                                    <th>Name</th>
+                                    <th>Project</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {EmpsUnderManager && EmpsUnderManager.map((emp,i)=>{
+                                    const handleEmpName=event=>{
+                                        const getEmpId=emp.id;
+                                        setGetEmpId(getEmpId);
+                                        setClickedEmpName(emp.name);
+                                        const getRoleName=loggedinUser.role.roleName;
+                                        setGetRole(getRoleName);
+                                        handleEmpNameOutside(getEmpId,getRoleName);
+                                        
+                                    }
+                                    return(
+                                        <tr>
+                                            <td onClick={handleEmpName} style={{cursor:"pointer"}}>{emp.name}</td>
+                                            <td>{emp.project.projName}</td>
+                                        </tr>
+                                    );
+                                })}
+                            </tbody>
+                        </table>
+                    </div>}
+                </div>
+                <div style={{ height: '570px', width: '700px', boxShadow: '0 0 2px 2px', marginLeft: '150px', borderRadius: '10px',backgroundColor:"#E4E4E4",overflow: "scroll"}}>
+                    <h3 style={{textAlign:"center"}}>{showManagerTasks&&clickedManagerName}</h3>
+                    <h3 style={{textAlign:"center"}}>{showEmpTasks&&clickedEmpName}</h3>
+                    <div style={{marginLeft:"120px"}}><table className="table table-hover">
+                        <thead style={{fontSize:"18px"}}>
+                            <tr>
+                                <th>Task</th>
+                                <th>Date</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {showManagerTasks && managerTasks && managerTasks.map((managerTask,i)=>{
+                                const handleApproveReject=event=>{
+                                    let status;
+                                    if(event.target.value==='1'){
+                                        status='Approved';
+                                    }
+                                    else if(event.target.value==='2'){
+                                        status='Rejected';
+                                    }
+                                    const clickedId=managerTask.id;
+                                    const roleName=loggedinUser.role.roleName;
+                                    const tasksRender=axios.put(`http://localhost:8001/ApproveORreject?taskId=${clickedId}&roleName=${roleName}&status=${status}`)
+                                    .then((res)=>{
+                                        handleManagerTasksOutside(stateManagerId);
+                                    })
+                                    .catch((error)=>{
+                                        console.log(error);
+                                    })
+                                }
+                                return(
+                                    <tr>
+                                        <td>{managerTask.tasks}</td>
+                                        <td>{managerTask.date}</td>
+                                        <tr>
+                                            <button value="1" className="btn btn-success" onClick={handleApproveReject}>Approve</button>
+                                            <button value="2" className="btn btn-danger" style={{marginLeft:"10px"}} onClick={handleApproveReject}>Reject</button>
+                                        </tr>
+                                    </tr>
+                                );
+                            })}
+                            {showEmpTasks && empTasksPendingAtAdmin && empTasksPendingAtAdmin.map((empTasksAtAdmin,i)=>{
+                                const handleApproveReject=event=>{
+                                    let status;
+                                    if(event.target.value==='1'){
+                                        status='Approved';
+                                    }
+                                    else if(event.target.value==='2'){
+                                        status='Rejected';
+                                    }
+                                    const clickedId=empTasksAtAdmin.id;
+                                    const roleName=loggedinUser.role.roleName;
+                                    const tasksRender=axios.put(`http://localhost:8001/ApproveORreject?taskId=${clickedId}&roleName=${roleName}&status=${status}`)
+                                    .then((res)=>{
+                                        handleEmpNameOutside(stateEmpId,stateRole);
+                                    })
+                                    .catch((error)=>{
+                                        console.log(error);
+                                    })
+                                }
+                                return(
+                                    <tr>
+                                        <td>{empTasksAtAdmin.tasks}</td>
+                                        <td>{empTasksAtAdmin.date}</td>
+                                        <tr>
+                                            <button value="1" className="btn btn-success" onClick={handleApproveReject}>Approve</button>
+                                            <button value="2" className="btn btn-danger" style={{marginLeft:"10px"}} onClick={handleApproveReject}>Reject</button>
+                                        </tr>
+                                    </tr>
+                                );
+                            })}
+                        </tbody>
+                    </table></div>
+                </div>
+            </div>
+            <ToastContainer autoClose={500} />
+        </div>
+    );
+}
+
+export default ManagerUnderAdmin;
